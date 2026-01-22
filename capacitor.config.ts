56505c1010d99d885d5ca1e0714767dc2d33b22e
iyose9203-afk <iyose9203@gmail.com>
2026-01-22 16:48:55 +0900

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.auto.site',
+  appName: 'AutoSite',
+  webDir: '.'
+};
+
+export default config;
